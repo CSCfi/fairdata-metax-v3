2e36283e1c1d952f219e15183de8af3a41559c15
Toni Nurmi <tonurmi@csc.fi>
2022-03-08 12:46:37 +0200

    Improve assert for publisher with homepage

--- tests/unit/apps/core/models/test_publisher.py
+++ tests/unit/apps/core/models/test_publisher.py
@@ -7,6 +7,7 @@ def test_create_publisher_with_webpage(catalog_homepage, dataset_publisher):
     dataset_publisher.homepage.add(catalog_homepage)
     assert dataset_publisher.id is not None
     assert dataset_publisher.homepage.count() != 0
+    assert dataset_publisher.homepage.get(id=catalog_homepage.id) == catalog_homepage
 
 
 def test_delete_with_web_page(dataset_publisher, catalog_homepage):
