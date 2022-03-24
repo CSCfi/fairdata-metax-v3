803eaaab28305e05ebdc10d88265672f4cbbefc4
Toni Nurmi <tonurmi@csc.fi>
2022-03-24 16:17:22 +0200

    bdd implementation

---
+++ tests/behave/features/datacatalog/datacatalog.feature
@@ -0,0 +1,9 @@
+# Created by tonurmi at 22.3.2022
+Feature: Data catalog
+  Creating and modifying Data Catalogs as Admin user
+
+  Scenario: Creating new DataCatalog
+    Given I'm an admin user
+    When I post a new DataCatalog to the datacatalog REST-endpoint
+    Then New DataCatalog object is saved to database
+    And It should return 201 http code
---
+++ tests/behave/features/datacatalog/steps_datacatalog.py
@@ -0,0 +1,21 @@
+from pytest_bdd import scenario, given, when, then
+
+
+@given("I'm an admin user")
+def admin_user():
+    pass
+
+
+@when("I post a new DataCatalog to the datacatalog REST-endpoint")
+def datacatalog_post_request():
+    raise NotImplementedError(u'STEP: When I post a new DataCatalog to the datacatalog REST-endpoint')
+
+
+@then("New DataCatalog object is saved to database")
+def check_datacatalog_is_created():
+    raise NotImplementedError(u'STEP: Then New DataCatalog object is saved to database')
+
+
+@given("It should return 201 http code")
+def step_impl():
+    raise NotImplementedError(u'STEP: And It should return 201 http code')
