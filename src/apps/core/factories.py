cb48b1780d077f117447dc11b8a06f1de36154e9
Toni Nurmi <tonurmi@csc.fi>
2022-06-21 16:25:58 +0300

    add more model factories, new management command for test data

--- src/apps/core/factories.py
+++ src/apps/core/factories.py
@@ -8,12 +8,50 @@ class AbstractDatasetPropertyFactory(factory.django.DjangoModelFactory):
     url = factory.Sequence(lambda n: f"example.com/{n}")
 
 
+class LanguageFactory(AbstractDatasetPropertyFactory):
+    class Meta:
+        model = models.DatasetLanguage
+        django_get_or_create = ("url",)
+
+
+class CatalogHomePageFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = models.CatalogHomePage
+        django_get_or_create = ("url",)
+
+
+class DatasetPublisherFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = models.DatasetPublisher
+        django_get_or_create = ("name",)
+
+
+class DatasetLicenseFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = models.DatasetLicense
+        django_get_or_create = ("url",)
+
+
+class AccessTypeFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = models.AccessType
+        django_get_or_create = ("url",)
+
+
+class AccessRightFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = models.AccessRight
+        django_get_or_create = ("description",)
+
+
 class DataCatalogFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = models.DataCatalog
+        django_get_or_create = ("title",)
 
     id = factory.Sequence(lambda n: f"urn:{n}")
     title = {"fi": "datacatalogi"}
+
 
 class FileFactory(factory.django.DjangoModelFactory):
     class Meta:
@@ -28,6 +66,7 @@ class DistributionFactory(factory.django.DjangoModelFactory):
 
     title = {"fi": "otsikko"}
     id = factory.Sequence(lambda n: f"distribution{n}")
+
 
 class DataStorageFactory(factory.django.DjangoModelFactory):
     class Meta:
---
+++ src/apps/core/management/commands/load_test_data.py
@@ -0,0 +1,49 @@
+import logging
+
+from django.core.management import call_command
+from django.core.management.base import BaseCommand
+from apps.core import factories
+
+logger = logging.getLogger(__name__)
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        language_fi = factories.LanguageFactory(
+            url="http://lexvo.org/id/iso639-3/fin",
+            title={"en": "Finnish", "fi": "suomi", "sv": "finska", "und": "suomi"},
+        )
+        homepage_fairdata = factories.CatalogHomePageFactory(
+            url="http://fairdata.fi", title={"en": "fairdata", "fi": "fairdata"}
+        )
+        dataset_publisher_csc = factories.DatasetPublisherFactory(
+            name={"fi": "CSC"}
+        )
+        dataset_publisher_csc.homepage.add(homepage_fairdata)
+        licence_cc_4_0 = factories.DatasetLicenseFactory(
+            title={
+                "en": "Creative Commons Attribution 4.0 International (CC BY 4.0)",
+                "fi": "Creative Commons Nimeä 4.0 Kansainvälinen (CC BY 4.0)",
+                "und": "Creative Commons Nimeä 4.0 Kansainvälinen (CC BY 4.0)",
+            },
+            url="http://uri.suomi.fi/codelist/fairdata/license/code/CC-BY-4.0",
+        )
+        access_type_open = factories.AccessTypeFactory(
+            title={"en": "Open", "fi": "Avoin", "und": "Avoin"},
+            url="http://uri.suomi.fi/codelist/fairdata/access_type/code/open",
+        )
+        access_rights = factories.AccessRightFactory(
+            description={
+                "en": "Contains datasets from Repotronic service",
+                "fi": "Sisältää aineistoja Repotronic-palvelusta",
+            },
+            access_type=access_type_open,
+            license=licence_cc_4_0,
+        )
+        data_catalog = factories.DataCatalogFactory(
+            title={"en": "Testing catalog", "fi": "Testi katalogi"},
+            access_rights=access_rights,
+            publisher=dataset_publisher_csc,
+        )
+        data_catalog.language.add(language_fi)
+        logger.info(data_catalog)
