59d9e3257c6910ba963364c58c8f4da4a684f51d
Pekka Tiainen <ari-pekka.tiainen@csc.fi>
2022-08-11 11:31:57 +0300

    Unit tests for DatasetLanguage CRUD API

--- tests/unit/apps/core/api/conftest.py
+++ tests/unit/apps/core/api/conftest.py
@@ -67,3 +67,57 @@ def post_datacatalog_payloads_a_b_c(client, datacatalog_a_json, datacatalog_b_js
     res3 = client.post(url, datacatalog_c_json, content_type='application/json')
     logger.info(f"{res1=}, {res2=}, {res3=}")
     return res1, res2, res3
+
+
+@pytest.fixture
+def dataset_language_fin_json():
+    with open(test_data_path + "dataset_language_fin.json") as json_file:
+        data = json.load(json_file)
+    return data
+
+
+@pytest.fixture
+def dataset_language_est_json():
+    with open(test_data_path + "dataset_language_est.json") as json_file:
+        data = json.load(json_file)
+    return data
+
+
+@pytest.fixture
+def dataset_language_spa_json():
+    with open(test_data_path + "dataset_language_spa.json") as json_file:
+        data = json.load(json_file)
+    return data
+
+
+@pytest.fixture
+def dataset_language_swe_json():
+    with open(test_data_path + "dataset_language_swe.json") as json_file:
+        data = json.load(json_file)
+    return data
+
+
+@pytest.fixture
+def dataset_language_put_fin_json():
+    with open(test_data_path + "dataset_language_put_fin.json") as json_file:
+        data = json.load(json_file)
+    return data
+
+
+@pytest.fixture
+def dataset_language_error_json():
+    with open(test_data_path + "dataset_language_error.json") as json_file:
+        data = json.load(json_file)
+    return data
+
+
+@pytest.fixture
+def post_dataset_languages(client, dataset_language_est_json, dataset_language_fin_json, dataset_language_spa_json, dataset_language_swe_json):
+    logger.info(__name__)
+    url = '/rest/v3/datasetlanguage'
+    res1 = client.post(url, dataset_language_est_json, content_type='application/json')
+    res2 = client.post(url, dataset_language_fin_json, content_type='application/json')
+    res3 = client.post(url, dataset_language_swe_json, content_type='application/json')
+    res4 = client.post(url, dataset_language_spa_json, content_type='application/json')
+    logger.info(f"{res1=}, {res2=}, {res3=}, {res4=}")
+    return res1, res2, res3, res4
