d5c87a4889814d3eaf74a8fad7a8bf225709b42d
Jori Niemi <3295718+tahme@users.noreply.github.com>
2023-12-15 14:58:15 +0200

    Remove automatic auth token generation

--- src/apps/users/apps.py
+++ src/apps/users/apps.py
@@ -4,5 +4,3 @@ from django.apps import AppConfig
 class UsersConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "apps.users"
-
-    from apps.users import signals
