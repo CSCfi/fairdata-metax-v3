95cb192b6d0550b190714dadf3f99dba36df8059
Jori Niemi <3295718+tahme@users.noreply.github.com>
2025-12-04 12:03:51 +0200

    Allow logging to "console" handler in non-debug mode

--- src/metax_service/settings/components/logging.py
+++ src/metax_service/settings/components/logging.py
@@ -34,15 +34,9 @@ LOGGING = {
             "style": "{",
         },
     },
-    "filters": {
-        "require_debug_true": {
-            "()": "django.utils.log.RequireDebugTrue",
-        }
-    },
     "handlers": {
         "console": {
             "level": "INFO",
-            "filters": ["require_debug_true"],
             "class": "logging.StreamHandler",
             "formatter": "simple",
         },
